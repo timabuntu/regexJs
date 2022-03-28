2ac4afe83157d3af17c6aaf16dae2150cff44918
thiago lopes de mello <timabuntu@gmail.com>
2022-03-28 11:48:53 -0300

    feat: base environment for study created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-regexJs
+# Expressões regulares 
+
+Este repositório foi criado para o estudo de Regex
---
+++ index.js
@@ -0,0 +1,8 @@
+// g - global (encontra todas as ocorrências)
+// i - insensitive
+// (()()) grupos
+// | ou
+
+const { texto } = require("./base");
+
+console.log(texto);
